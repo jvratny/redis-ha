2be3d72a1ae9e50b18ec4a418ab24b8a066d0b26
Jaromír Vrátný <jvratny@gmail.com>
2019-10-26 21:31:47 +0200

    changed post-init.sh script to expose service instead pod

---
+++ post-init.sh
@@ -0,0 +1,9 @@
+# This file serves for extending the container image, typically by changing
+# the configuration, loading some data etc.
+
+# Feel free to add content to this file or rewrite it at all.
+# You may also start redis server locally to load some data for example,
+# but do not forget to stop it after it, so it can be restarted after it.
+echo "\n" >> /etc/redis.conf
+eval echo "cluster-announce-ip \$$REDIS_INSTANCE""_SERVICE_HOST" >> /etc/redis.conf
+echo "\n" >> /etc/redis.conf
